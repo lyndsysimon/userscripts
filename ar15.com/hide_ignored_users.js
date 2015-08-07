90a5308eabfd5f7236a756997c2467ce03cd2691
Lyndsy Simon <lyndsy@lyndsysimon.com>
2015-08-06 20:25:38 -0400

    Add a script to hide hammermill threads...

---
+++ ar15.com/hide_ignored_users.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Hide threads by ignored users
+// @namespace    https://github.com/lyndsysimon/userscripts/
+// @version      0.1
+// @description  enter something useful
+// @author       Lyndsy Simon
+// @match        http://www.ar15.com/*
+// @grant        none
+// ==/UserScript==
+
+// Remove topics from ignored users
+$('.forumBox + .repliesBox[style*=646464]').parent().remove()
+
+// Re-apply classes to alternate row colors
+$('div[class*=forumTable][class!=forumTableDarkNews][class!=forumTableLightNews]').each(function(idx, elem) {
+    var e = $(elem);
+    if (idx % 2 == 0) {
+        if (e.hasClass('forumTableLight')) {
+            $(elem).removeClass('forumTableLight');
+            $(elem).addClass('forumTableDark');
+        }
+    } else {
+        if (e.hasClass('forumTableDark')) {
+            $(elem).removeClass('forumTableDark');
+            $(elem).addClass('forumTableLight');
+        }
+    }
+});
